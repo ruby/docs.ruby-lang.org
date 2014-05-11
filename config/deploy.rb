63f3487371852092a07c178f2f758605989e94da
SHIBATA Hiroshi <hsbt@ruby-lang.org>
2014-05-11 22:22:51 +0900

    specified ruby version for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,6 @@ set :application, 'docs.ruby-lang.org'
 set :repo_url, 'git@github.com:ruby/docs.ruby-lang.org.git'
 set :deploy_to, '/var/www/docs.ruby-lang.org'
 set :linked_dirs, %w{sources en/1.8.7 en/1.9.3 en/2.0.0 en/2.1.0 en/trunk ja/1.8.7 ja/1.9.3 ja/2.0.0 ja/2.1.0}
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.1.2'
