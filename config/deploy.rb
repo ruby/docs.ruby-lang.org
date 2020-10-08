a045a214a37560d7a1a69d91987e07e4008d0b58
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2020-10-08 21:55:09 +0900

    Set DEBIAN_DISABLE_RUBYGEMS_INTEGRATION

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,7 @@ set :deploy_to, '/var/www/docs.ruby-lang.org'
 
 set :default_env, {
   'PATH' => '/snap/bin:$PATH',
+  'DEBIAN_DISABLE_RUBYGEMS_INTEGRATION' => 'true',
 }
 # TODO: Add 3.0.0 after 2.8.0 is changed to 3.0.0
 versions = %w[1.8.7 1.9.3 2.0.0 2.1.0 2.2.0 2.3.0 2.4.0 2.5.0 2.6.0 2.7.0 master]
